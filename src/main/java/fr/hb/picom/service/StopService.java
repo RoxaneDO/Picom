c3157c4c4c422ce87bbddcb3dca38047cb9764bb
RoxaneDO <roxane.donnat@gmail.com>
2022-06-27 16:38:26 +0200

    mise en place des service et impl

---
+++ src/main/java/fr/hb/picom/service/StopService.java
@@ -0,0 +1,10 @@
+package fr.hb.picom.service;
+
+import fr.hb.picom.business.Stop;
+
+import java.util.List;
+
+public interface StopService {
+    Stop getStop(Long id);
+    List<Stop> getStops();
+}
---
+++ src/main/java/fr/hb/picom/service/impl/StopServiceImpl.java
@@ -0,0 +1,20 @@
+package fr.hb.picom.service.impl;
+
+import fr.hb.picom.business.Stop;
+import fr.hb.picom.service.StopService;
+
+import java.util.List;
+
+public class StopServiceImpl implements StopService {
+
+
+    @Override
+    public Stop getStop(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Stop> getStops() {
+        return null;
+    }
+}

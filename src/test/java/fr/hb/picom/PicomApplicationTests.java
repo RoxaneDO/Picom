638497834984c9fb85d74eba9b2eaecaad0a9b43
RoxaneDO <roxane.donnat@gmail.com>
2022-06-27 15:08:27 +0200

    first commit

---
+++ src/test/java/fr/hb/picom/PicomApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hb.picom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

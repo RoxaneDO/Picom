5f8ecb99220d70ca2c7790c98599671704fc85e5
Marianne L <laballery.marianne@orange.fr>
2022-06-28 10:52:31 +0200

    zonecontroller

--- src/main/java/fr/hb/picom/controller/ZoneController.java
+++ src/main/java/fr/hb/picom/controller/ZoneController.java
@@ -1,10 +1,27 @@
 package fr.hb.picom.controller;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.RestController;
+import fr.hb.picom.business.Zone;
+import fr.hb.picom.service.ZoneService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.AllArgsConstructor;
 
 @RestController
+@RequestMapping("/api")
+@AllArgsConstructor
 public class ZoneController {
 
+    private final ZoneService zoneService;
 
+    @GetMapping("zones")
+    public List<Zone> zonesGet() { return zoneService.getZones();}
 
+    @GetMapping("zones/{id}")
+    public Zone zonesGet(@PathVariable Integer id) { return  zoneService.getZone(id);}
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("zones")
+    public Zone zonesPost(@RequestBody Zone zone) { return zoneService.addZone(zone);}
 }
